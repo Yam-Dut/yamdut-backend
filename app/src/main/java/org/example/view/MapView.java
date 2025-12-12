b4ed326a903cada612bfa7ddbf027e5640d533e0
abhishekk1004 <abhishekkushwaha.np@gmail.com>
2025-12-12 09:30:20 +0545

    feat: Map View

--- app/src/main/java/org/example/view/MapView.java
+++ app/src/main/java/org/example/view/MapView.java
@@ -6,22 +6,22 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-import java.net.URL;
-
 public class MapView extends Application {
 
     @Override
     public void start(Stage stage) {
-
         WebView webView = new WebView();
         WebEngine engine = webView.getEngine();
 
-        URL url = getClass().getResource("/web/map.html");
-        engine.load(url.toExternalForm());
+        var url = getClass().getResource("/web/map.html");
+        if (url == null) {
+            engine.loadContent("<h1>map.html not found</h1>");
+        } else {
+            engine.load(url.toExternalForm());
+        }
 
-        Scene scene = new Scene(webView, 1200, 800);
-        stage.setTitle("Yamdut - Live Ride Tracking");
-        stage.setScene(scene);
+        stage.setScene(new Scene(webView, 1200, 800));
+        stage.setTitle("Yamdut - Live Tracking");
         stage.show();
     }
 }
