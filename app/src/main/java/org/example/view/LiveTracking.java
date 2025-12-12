6964e5258f31877122cefb85e609c2992e8f27d2
abhishekk1004 <abhishekkushwaha.np@gmail.com>
2025-12-12 09:30:20 +0545

    feat: Live Tracking

--- app/src/main/java/org/example/view/LiveTracking.java
+++ app/src/main/java/org/example/view/LiveTracking.java
@@ -1,5 +1,33 @@
 package org.example.view;
 
+import javafx.scene.web.WebEngine;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class LiveTracking {
-    public static final String WEBSOCKET_URL = "http://localhost:8080/trip";
+
+    private final WebEngine engine;
+    private double lat = 27.7172;
+    private double lng = 85.3240;
+
+    public LiveTracking(WebEngine engine) {
+        this.engine = engine;
+    }
+
+    public void startTracking() {
+        Timer timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                lat += 0.0002;
+                lng += 0.0002;
+
+                engine.executeScript(
+                    "updateDriverLocation(" + lat + "," + lng + ");"
+                );
+            }
+        }, 3000, 3000);
+    }
 }
