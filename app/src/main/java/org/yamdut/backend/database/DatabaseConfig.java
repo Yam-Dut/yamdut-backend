32c44e17aa3c59e5fda2ecb7fa062c80631ba8dc
amrxtgh <ghimireamrit619@gmail.com>
2025-12-15 04:45:30 +0545

    change: file name mismatched

---
+++ app/src/main/java/org/yamdut/backend/database/DatabaseConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.yamdut.backend.database;
+import java.sql.*;
+
+
+/**
+ *
+ * @author dibij
+ */
+public interface DatabaseConfig {
+    Connection openConnection();
+    void closeConnection (Connection conn);
+    ResultSet runQuery(Connection conn, String query);
+    int executeUpdate(Connection conn, String query);
+}
