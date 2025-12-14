cacd1dcbd80b388990d8d762885efd313815e036
amrxtgh <ghimireamrit619@gmail.com>
2025-12-15 04:47:01 +0545

    mischallenous

---
+++ app/src/main/java/org/yamdut/controller/UserDashboardController.java
@@ -0,0 +1,11 @@
+package org.yamdut.controller;
+
+import org.yamdut.view.UserDashboardView;
+
+public class UserDashboardController {
+    private UserDashboardView view;
+
+    public UserDashboardController(UserDashboardView view) {
+        this.view = view;
+    }
+}
