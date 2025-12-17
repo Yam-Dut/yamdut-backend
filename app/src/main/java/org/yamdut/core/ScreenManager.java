8d474c709c22b481681aec5920e3ffe231baba8d
amrxtgh <ghimireamrit619@gmail.com>
2025-12-17 06:22:14 +0545

    add: ScreenManager.java

--- app/src/main/java/org/yamdut/core/ScreenManager.java
+++ app/src/main/java/org/yamdut/core/ScreenManager.java
@@ -1,5 +1,9 @@
 package org.yamdut.core;
 
+import org.yamdut.ui.signup.*;
+import org.yamdut.backend.service.AuthService;
+import org.yamdut.backend.model.User;
+
 
 import javax.swing.*;
 import java.util.HashMap;
@@ -25,5 +29,24 @@ public class ScreenManager {
         frame.setContentPane(panel);
         frame.revalidate();
         frame.repaint();
+    }
+    public void showOtpScreen(User user, boolean isSignup) {
+        OtpScreen otpScreen = new OtpScreen(user, isSignup, this);
+        
+        // Generate and send OTP
+        AuthService authService = new AuthService();
+        String otp = authService.generateAndSendOtp(
+            user.getEmail(), 
+            isSignup ? "signup" : "login"
+        );
+        
+        // Store the OTP in service
+        // (Your AuthService should handle this)
+        
+        // Show OTP screen
+        String screenName = "OTP_" + user.getEmail();
+        screens.put(screenName, otpScreen);
+        mainPanel.add(otpScreen, screenName);
+        show(screenName);
     }
 }
