97037df67fe604d43c8c7c6250b23371c3eb76b1
amrxtgh <ghimireamrit619@gmail.com>
2025-12-19 01:52:27 +0545

    refactor: imports and all

--- app/src/main/java/org/yamdut/controller/OtpController.java
+++ app/src/main/java/org/yamdut/controller/OtpController.java
@@ -3,32 +3,28 @@ package org.yamdut.controller;
 import org.yamdut.backend.service.OtpService;
 import org.yamdut.backend.service.UserService;
 import org.yamdut.core.ScreenManager;
-import org.yamdut.utils.UserSession;
+// import org.yamdut.utils.UserSession;
 import org.yamdut.backend.model.User;
 
 public class OtpController {
     private final UserService userService;
-    private final ScreenManager screenManager;
+    // private final ScreenManager screenManager;
     private final OtpService otpService;
     
 
     public OtpController(ScreenManager screenManager) {
         this.userService = new UserService();
-        this.screenManager = screenManager;
+        // this.screenManager = screenManager;
         this.otpService = OtpService.getInstance();
     }
 
 
     public boolean verify(User user, String otp, boolean isSignup) {
+        OtpService otpService = OtpService.getInstance();
         boolean valid = otpService.verifyOtp(user.getEmail(), otp);
 
-        if (valid) {
-            if (isSignup) {
+        if (valid && isSignup) {
                 userService.activateUser(user.getEmail());
-            }
-            UserSession.getInstance().login(user);
-
-            screenManager.showDashBoardForRole(user.getRole());
         }
         return valid;
     }
