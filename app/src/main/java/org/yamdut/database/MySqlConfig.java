45d0c488fa2a136760cd3329f614e1238ea2f4b0
amrxtgh <ghimireamrit619@gmail.com>
2025-12-21 03:24:15 +0545

    add: mysql config

--- app/src/main/java/org/yamdut/database/MySqlConfig.java
+++ app/src/main/java/org/yamdut/database/MySqlConfig.java
@@ -5,18 +5,32 @@ import java.sql.DriverManager;
 import java.sql.*;
 
 public class MySqlConfig {
+    // for linux/mariadb (amrxtgh69)
     private static final String URL = "jdbc:mariadb://localhost:3306/yamdut_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Rolex123";
+    private static final String USER = "yamdut";
+    private static final String PASSWORD = "//your password here";
+
+    //for windows 
+    // private static final String URL = "jdbc:mysql://localhost:3306/yamdut_db";
+    // private static final String USER = "your username here";
+    // private static final String PASSWORD = "your password here";
 
     static {
         try {
+            // for linux
             Class.forName("org.mariadb.jdbc.Driver");
+            //for windows
+            //Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Mariadb driver not found, e");
+            throw new RuntimeException("Mariadb driver not found", e);
         }
     }
-    public static Connection gConnection() throws SQLException {
+    /*
+    ===========================================================================
+    Get connection
+    ===========================================================================
+     */
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
