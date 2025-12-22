3d24233833820f4fd48f0a44bac841f2c0263d43
amrxtgh <ghimireamrit619@gmail.com>
2025-12-22 23:37:53 +0545

    add: implement fetchRoute method in MapService class

--- app/src/main/java/org/yamdut/service/MapService.java
+++ app/src/main/java/org/yamdut/service/MapService.java
@@ -1,8 +1,16 @@
 package org.yamdut.service;
 
+import java.nio.channels.UnsupportedAddressTypeException;
+
+import org.jxmapviewer.viewer.GeoPosition;
 import org.jxmapviewer.viewer.Waypoint;
+
+import java.util.List;
 
 public class MapService {
   //OSRM integration
-  
+  public List<GeoPosition> fetchRoute(List<GeoPosition> points) {
+    // TODO: call OSRM
+    throws new UnsupportedAddressException("OSRM not wired yet");
+  }
 }
