f44cb0463419e0945f283c6e849cc725dc08e0a9
amrxtgh <ghimireamrit619@gmail.com>
2025-12-22 19:35:22 +0545

    refactor: update  with clearer user and password placeholders

--- app/src/main/java/org/yamdut/database/MySqlConfig.java
+++ app/src/main/java/org/yamdut/database/MySqlConfig.java
@@ -5,31 +5,31 @@ import java.sql.DriverManager;
 import java.sql.*;
 
 public class MySqlConfig {
+   
     // for linux/mariadb (amrxtgh69)
-    private static final String URL = "jdbc:mariadb://localhost:3306/yamdut_db";
-    private static final String USER = "yamdut";
-    private static final String PASSWORD = "//your password here";
 
-    //for windows 
-    // private static final String URL = "jdbc:mysql://localhost:3306/yamdut_db";
-    // private static final String USER = "your username here";
-    // private static final String PASSWORD = "your password here";
+    /*
+    i have add mariadb for myself u do yourself 
+     */
+    
+    private static final String URL = "jdbc:mariadb://localhost:3306/yamdut_db";
+    private static final String USER = "your user name";
+    private static final String PASSWORD = "your password here boss";
 
     static {
         try {
             // for linux
+           
             Class.forName("org.mariadb.jdbc.Driver");
-            //for windows
+           
+            /*  for windows uncomment the line below */
+           
             //Class.forName("com.mysql.cj.jdbc.Driver");
+        
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Mariadb driver not found", e);
         }
     }
-    /*
-    ===========================================================================
-    Get connection
-    ===========================================================================
-     */
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
