1ba83f222f592c7696a6cca2ebddadcfbbc2a93e
amrxtgh <ghimireamrit619@gmail.com>
2025-12-22 23:34:41 +0545

    refactor: remove Waypoint implementation and unused attributes from MapService class

--- app/src/main/java/org/yamdut/service/MapService.java
+++ app/src/main/java/org/yamdut/service/MapService.java
@@ -1,13 +1,8 @@
 package org.yamdut.service;
 
-import org.jxmapviewer.viewer.GeoPosition;
 import org.jxmapviewer.viewer.Waypoint;
-import org.yamdut.model.MapEntityType;
 
-public class MapService implements Waypoint {
-
-  private final GeoPosition position;
-  private final MapEntityType type;
-
+public class MapService {
+  //OSRM integration
   
 }
