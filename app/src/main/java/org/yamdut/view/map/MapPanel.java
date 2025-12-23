1b9201f1100b7a3b624bc69e0333292fdb5e22b8
amrxtgh <ghimireamrit619@gmail.com>
2025-12-23 23:50:42 +0545

    add: integrate map.html and calling the JS function

--- app/src/main/java/org/yamdut/view/map/MapPanel.java
+++ app/src/main/java/org/yamdut/view/map/MapPanel.java
@@ -5,7 +5,9 @@ package org.yamdut.view.map;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
-
+import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
+import javafx.scene.web.WebEngine;
 
 import javax.swing.*;
 import java.awt.*;
@@ -22,8 +24,58 @@ public class MapPanel extends JPanel {
     private WebEngine webEngine;
 
     public MapPanel() {
-        setLayout(new BorderLayout);
+        setLayout(new BorderLayout());
         fxPanel = new JFXPanel();
         add(fxPanel, BorderLayout.CENTER);
+        
+        initFX();
+    }
+
+    private void initFX() {
+        Platform.runLater(() -> {
+            WebView webView = new WebView();
+            webEngine = webView.getEngine();
+
+            //Loading the map.html
+            String url = getClass().getResource("/map/map.html").toExternalForm();
+            webEngine.load(url);
+            
+            webEngine.getLoadWorker().stateProperty().addListener((obs, oldState, newState) -> {
+                if (newState == javafx.concurrent.Worker.State.SUCCEEDED) {
+                    JSObject window = (JSObject) webEngine.executeScript("window");
+                    window.setMember("javaConnector", new JavaConnector());
+                }
+            });
+
+            fxPanel.setScene(new Scene(webView));
+        });
+    }
+    /** 
+     show entities (driver passenger on the map)
+     @param jsonData JSON string [{id:"1", name:"Driver1", lat:"78.32", lon:"2380.9", type:"driver"}....]
+     * **/
+    public void showEntities(String jsonData) {
+        Platform.runLater(() -> {
+            webEngine.executeScript("YamdutMap.showEntites(" + jsonData + ");");
+        });
+    }
+    
+    /** 
+     @param routeJson JSON array string [{lat:"12.4", lon:"23.53"}]
+     **/
+    public void drawRoute(String routeJSon) {
+        Platform.runLater(() -> webEngine.executeScript("YamdutMap.startSimulation();"));
+    }
+
+    public void stopSimulation() {
+        Platform.runLater(() -> webEngine.executeScript("YamdutMap.stopSimulation();"));
+    }
+
+
+
+    public static class JavaConnector {
+        public void recieveMessage(String msg) {
+            System.out.println("JS says" + msg);
+        }
     }
 }
