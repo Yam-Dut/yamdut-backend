389dce7ee8868c60af194ad9311b6a79899ef2f8
amrxtgh <ghimireamrit619@gmail.com>
2025-12-28 02:00:20 +0545

    todo this is bs

--- app/src/main/java/org/yamdut/controller/PassengerDashboardController.java
+++ app/src/main/java/org/yamdut/controller/PassengerDashboardController.java
@@ -16,59 +16,6 @@ public class PassengerDashboardController {
 
     public PassengerDashboardController(PassengerDashboard view) {
         this.view = view;
-        initController();
-    }
-
-    private void initController() {
-        view.getBookRideButton().addActionListener(e -> submitRideRequest());
-        view.getLogoutButton().addActionListener(e -> logout());
-    }
-
-    private void submitRideRequest() {
-        String pickup = view.getPickupField().getText();
-        String destination = view.getDestinationField().getText();
-
-        if (pickup.isEmpty() || destination.isEmpty()) {
-            JOptionPane.showMessageDialog(view, "Enter pickup and destination");
-            return;
-        }
-
-        currentRequest = new RideRequest(pickup, destination);
-        RideMatchingService.submit(currentRequest);
-
-        showWaitingUI();
-        waitForDriverAcceptance();
-    }
-
-    private void showWaitingUI() {
-        JPanel routePanel = view.getRoutePanel();
-        routePanel.removeAll();
-        routePanel.add(new JLabel("Waiting for a driver to accept..."));
-        routePanel.revalidate();
-        routePanel.repaint();
-    }
-
-    private void waitForDriverAcceptance() {
-        Timer timer = new Timer(1000, e -> {
-            if (currentRequest.isAccepted()) {
-                ((Timer) e.getSource()).stop();
-                simulateRoute();
-            }
-        });
-        timer.start();
-    }
-
-    private void simulateRoute() {
-        JPanel routePanel = view.getRoutePanel();
-        routePanel.removeAll();
-
-        routePanel.add(new JLabel(
-                "Driver " + currentRequest.getAcceptedDriver()
-                        + " is on the way to " + currentRequest.getPickup()
-        ));
-
-        routePanel.revalidate();
-        routePanel.repaint();
     }
 
     private void logout() {
