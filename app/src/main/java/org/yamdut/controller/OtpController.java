da792051a92027ef9f833761dc094c25bb376b13
amrxtgh <ghimireamrit619@gmail.com>
2026-01-02 01:06:42 +0545

    forget

--- app/src/main/java/org/yamdut/controller/OtpController.java
+++ app/src/main/java/org/yamdut/controller/OtpController.java
@@ -9,26 +9,33 @@ public class OtpController {
     private final UserService userService;
     // private final ScreenManager screenManager;
     private final OtpService otpService;
-    
+    private final org.yamdut.service.EmailService emailService;
 
     public OtpController(ScreenManager screenManager) {
         this.userService = new UserService();
         // this.screenManager = screenManager;
         this.otpService = OtpService.getInstance();
+        this.emailService = new org.yamdut.service.EmailService();
     }
 
-
     public boolean verify(User user, String otp, boolean isSignup) {
-        boolean valid = otpService.verifyOtp(user.getEmail(), otp);
+        boolean valid = otpService.verifyOtp(user.getEmail(), otp, org.yamdut.model.OtpPurpose.SIGNUP);
 
         if (valid && isSignup) {
             userService.activateUser(user.getEmail());
-            otpService.clearOtp(user.getEmail());
+            otpService.clearOtp(user.getEmail(), org.yamdut.model.OtpPurpose.SIGNUP);
         }
         return valid;
     }
 
     public boolean resendOtp(User user) {
-        return otpService.resendOtp(user.getEmail());
+        String otp = otpService.generateOtp(user.getEmail(), org.yamdut.model.OtpPurpose.SIGNUP);
+        try {
+            emailService.sendOtpEmail(user.getEmail(), otp);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
